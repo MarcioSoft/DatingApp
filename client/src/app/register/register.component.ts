e039881f9a4681d54f653897267a69d35c89c841
MarcioSoft <87972336+MarcioSoft@users.noreply.github.com>
2021-08-05 22:18:11 +0100

    cap.12 - 141

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,7 +1,7 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
 import { AccountService } from '../_services/account.service';
-
 
 @Component({
   selector: 'app-register',
@@ -11,20 +11,29 @@ import { AccountService } from '../_services/account.service';
 export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
   model: any = {};
+  registerForm: FormGroup;
 
   constructor(private accountService: AccountService, private toastr: ToastrService) { }
 
   ngOnInit(): void {
+    this.intitializeForm();
+  }
+
+  intitializeForm() {
+    this.registerForm = new FormGroup({
+      username: new FormControl('Hello', Validators.required),
+      password: new FormControl('', [Validators.required, 
+        Validators.minLength(4),  Validators.maxLength(8)]),
+      confirmPassword: new FormControl('', Validators.required)
+    })  
   }
 
   register() {
-    this.accountService.register(this.model).subscribe(response => {
-      console.log(response);
-      this.cancel();
-    }, error => {
-      console.log(error);
-      this.toastr.error(error.error);
-    })
+    console.log(this.registerForm.value);
+    //  console.log(response);
+    //  console.log(error);
+    //  this.toastr.error(error.error);
+    //})
   }
 
   cancel() {
