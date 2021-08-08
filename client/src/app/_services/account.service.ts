9cb3126249dd105e36fe5145f45659beffe95e3f
MarcioSoft <87972336+MarcioSoft@users.noreply.github.com>
2021-08-08 18:20:12 +0100

    half 16

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import {map} from 'rxjs/operators';
+import { map } from 'rxjs/operators';
 import { ReplaySubject } from 'rxjs';
 import { User } from '../_models/user';
 import { environment } from 'src/environments/environment';
@@ -38,6 +38,9 @@ export class AccountService {
   }
 
   setCurrentUser(user: User) {
+    user.roles = [];
+    const roles = this.getDecodedToken(user.token).role;
+    Array.isArray(roles) ? user.roles = roles : user.roles.push(roles);
     localStorage.setItem('user', JSON.stringify(user));
     this.currentUserSource.next(user);
   }
@@ -47,5 +50,9 @@ export class AccountService {
     localStorage.removeItem('user');
     this.currentUserSource.next(null);
  
+  }
+
+  getDecodedToken(token) {
+    return JSON.parse(atob(token.split('.')[1]));
   }
 }
